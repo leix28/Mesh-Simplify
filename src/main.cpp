a386a6bd735fe9ac4837f226481da99c2cb4b3a8
LazyLie <leopard.lie@gmail.com>
2015-06-15 00:20:01 +0800

    define some type

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,36 @@
 #include <algorithm>
 #define BUFFER_SIZE 1024
 #define INFD 1e8
+using std::min;
+using std::max;
+using std::make_pair;
 
-void printVector(const std::vector<double> &v) {
+typedef std::vector<double> Vector;
+typedef std::vector<Vector> Matrix;
+typedef std::pair<int, int> Edge;
+
+void printVector(const Vector &v) {
   for (auto x : v)
     printf("%.4lf\t", x);
   printf("\n");
 }
 
-double norm(const std::vector<double> &v) {
+double norm(const Vector &v) {
   double t = 0;
   for (auto x : v) t += x * x;
   return sqrt(t);
 }
 
-std::vector<double> crossProduct(const std::vector<double> &a, const std::vector<double> &b) {
+Vector crossProduct(const Vector &a, const Vector &b) {
   assert(a.size() == 3 && b.size() == 3);
-  std::vector<double> c(3);
+  Vector c(3);
   c[0] = a[1] * b[2] - a[2] * b[1];
   c[1] = a[2] * b[0] - a[0] * b[2];
   c[2] = a[0] * b[1] - a[1] * b[0];
   return c;
 }
 
-double innerProduct(const std::vector<double> &a, const std::vector<double> &b) {
+double innerProduct(const Vector &a, const Vector &b) {
   assert(a.size() == b.size());
   double c = 0;
   for (int i = 0; i < a.size(); i++)
@@ -37,43 +44,43 @@ double innerProduct(const std::vector<double> &a, const std::vector<double> &b) 
   return c;
 }
 
-std::vector< std::vector<double> > outerProduct(const std::vector<double> &a, const std::vector<double> &b) {
-  std::vector< std::vector<double> > c(a.size(), std::vector<double>(b.size(), 0));
+Matrix outerProduct(const Vector &a, const Vector &b) {
+  Matrix c(a.size(), Vector(b.size(), 0));
   for (int i = 0; i < a.size(); i++)
     for (int j = 0; j < b.size(); j++)
       c[i][j] = a[i] * b[j];
   return c;
 }
 
-std::vector<double> innerProduct(const std::vector<double> &a, const std::vector< std::vector<double> > &b) {
+Vector innerProduct(const Vector &a, const Matrix &b) {
   assert(a.size() == b.size());
-  if (a.size() == 0) return std::vector<double>();
-  std::vector<double> c(b[0].size(), 0);
+  if (a.size() == 0) return Vector();
+  Vector c(b[0].size(), 0);
   for (int i = 0; i < b.size(); i++)
     for (int j = 0; j < b[0].size(); j++)
       c[j] += a[i] * b[i][j];
   return c;
 }
 
-std::vector<double> operator + (const std::vector<double> &a, const std::vector<double> &b) {
+Vector operator + (const Vector &a, const Vector &b) {
   assert(a.size() == b.size());
-  std::vector<double> c(a.size());
+  Vector c(a.size());
   for (int i = 0; i < a.size(); i++)
     c[i] = a[i] + b[i];
   return c;
 }
 
-std::vector<double> operator - (const std::vector<double> &a, const std::vector<double> &b) {
+Vector operator - (const Vector &a, const Vector &b) {
   assert(a.size() == b.size());
-  std::vector<double> c(a.size());
+  Vector c(a.size());
   for (int i = 0; i < a.size(); i++)
     c[i] = a[i] - b[i];
   return c;
 }
 
-std::vector<double> operator / (const std::vector<double> &a, const double &b) {
+Vector operator / (const Vector &a, const double &b) {
   assert(b != 0);
-  std::vector<double> c(a.size());
+  Vector c(a.size());
   for (int i = 0; i < a.size(); i++)
     c[i] = a[i] / b;
   return c;
@@ -83,13 +90,13 @@ std::vector<double> operator / (const std::vector<double> &a, const double &b) {
 
 
 class Model {
-  std::vector< std::vector<double> > vertex;
+  std::vector< Vector > vertex;
   std::vector<bool> removed;
-  std::vector< std::set< std::pair<int, int> > > face;
-  std::set< std::pair<int, int> > edge;
+  std::vector< std::set<Edge> > face;
+  std::set<Edge> edge;
   int faceN;
 
-  double edgeLen(std::pair<int, int> e) {
+  double edgeLen(Edge e) {
     return norm(vertex[e.first] - vertex[e.second]);
   }
 
@@ -115,7 +122,7 @@ public:
   }
 
   void selfCheck() {
-    std::set< std::pair<int, int> > ss;
+    std::set<Edge> ss;
 
     int vertexN = getVertexN();
     for (int i = 0; i < vertexN; i++) {
@@ -124,9 +131,9 @@ public:
       } else {
         for (const auto &x : face[i]) {
           assert(!removed[x.first] && !removed[x.second]);
-          assert(face[x.first].find(std::make_pair(x.second, i)) != face[x.first].end());
-          assert(face[x.second].find(std::make_pair(i, x.first)) != face[x.second].end());
-          ss.insert(std::make_pair(std::min(x.first, x.second), std::max(x.first, x.second)));
+          assert(face[x.first].find(make_pair(x.second, i)) != face[x.first].end());
+          assert(face[x.second].find(make_pair(i, x.first)) != face[x.second].end());
+          ss.insert(make_pair(min(x.first, x.second), max(x.first, x.second)));
         }
       }
     }
@@ -155,7 +162,7 @@ public:
 
 
     int vertexN = vertexIn.size();
-    vertex.resize(vertexN, std::vector<double>(3, 0));
+    vertex.resize(vertexN, Vector(3, 0));
     removed.resize(vertexN, false);
     face.resize(vertexN);
     faceN = faceIn.size();
@@ -169,14 +176,14 @@ public:
       int v[3];
       sscanf(f.c_str(), "%*s%d%d%d", v, v + 1, v + 2);
       v[0] --; v[1] --; v[2] --;
-      face[v[0]].insert(std::make_pair(v[1], v[2]));
-      face[v[1]].insert(std::make_pair(v[2], v[0]));
-      face[v[2]].insert(std::make_pair(v[0], v[1]));
+      face[v[0]].insert(make_pair(v[1], v[2]));
+      face[v[1]].insert(make_pair(v[2], v[0]));
+      face[v[2]].insert(make_pair(v[0], v[1]));
       std::sort(v, v + 3);
       assert(0 <= v[0] && v[0] < v[1] && v[1] < v[2] && v[2] < vertexN);
-      edge.insert(std::make_pair(v[0], v[1]));
-      edge.insert(std::make_pair(v[1], v[2]));
-      edge.insert(std::make_pair(v[0], v[2]));
+      edge.insert(make_pair(v[0], v[1]));
+      edge.insert(make_pair(v[1], v[2]));
+      edge.insert(make_pair(v[0], v[2]));
     }
   }
 
@@ -204,7 +211,7 @@ public:
     }
   }
 
-  double getCost(int vid, std::vector<double> vpos) {
+  double getCost(int vid, Vector vpos) {
     auto v = vertex[vid] - vpos;
     // printVector(v);
     double cost = 0;
@@ -218,8 +225,8 @@ public:
     return cost;
   }
 
-  std::pair<int, int> selectEdge(double threshold) {
-    std::pair<int, int> idx = std::make_pair(-1, -1);
+  Edge selectEdge(double threshold) {
+    Edge idx = make_pair(-1, -1);
     double best = INFD;
     for (const auto &e : edge) {
       if (edgeLen(e) > threshold) continue;
@@ -231,12 +238,12 @@ public:
         idx = e;
       }
     }
-    assert(idx != std::make_pair(-1, -1));
+    assert(idx != make_pair(-1, -1));
     printf("%lf %d %d", best, idx.first, idx.second);
     return idx;
   }
 
-  void removeEdge(std::pair<int, int> e) {
+  void removeEdge(Edge e) {
     auto v = (vertex[e.first] + vertex[e.second]) / 2.0;
     edge.erase(e);
     vertex[e.first] = v;
@@ -244,16 +251,16 @@ public:
     removed[e.second] = true;
 
     for (const auto &f : face[e.second]) {
-      assert(face[f.second].find(std::make_pair(e.second, f.first)) != face[f.second].end());
-      face[f.second].erase(std::make_pair(e.second, f.first));
+      assert(face[f.second].find(make_pair(e.second, f.first)) != face[f.second].end());
+      face[f.second].erase(make_pair(e.second, f.first));
       if (f.first != e.first && f.second != e.first) {
-        face[f.second].insert(std::make_pair(e.first, f.first));
+        face[f.second].insert(make_pair(e.first, f.first));
       }
 
-      assert(face[f.first].find(std::make_pair(f.second, e.second)) != face[f.first].end());
-      face[f.first].erase(std::make_pair(f.second, e.second));
+      assert(face[f.first].find(make_pair(f.second, e.second)) != face[f.first].end());
+      face[f.first].erase(make_pair(f.second, e.second));
       if (f.first != e.first && f.second != e.first) {
-        face[f.first].insert(std::make_pair(f.second, e.first));
+        face[f.first].insert(make_pair(f.second, e.first));
       }
 
       if (f.first == e.first || f.second == e.first)
@@ -261,15 +268,15 @@ public:
       else
         face[e.first].insert(f);
 
-      auto tmp = std::make_pair(std::min(e.second, f.first), std::max(e.second, f.first));
+      auto tmp = make_pair(min(e.second, f.first), max(e.second, f.first));
       if (edge.find(tmp) != edge.end())
         edge.erase(tmp);
-      tmp = std::make_pair(std::min(e.second, f.second), std::max(e.second, f.second));
+      tmp = make_pair(min(e.second, f.second), max(e.second, f.second));
       if (edge.find(tmp) != edge.end())
         edge.erase(tmp);
       if (f.first != e.first && f.second != e.first) {
-        edge.insert(std::make_pair(std::min(e.first, f.first), std::max(e.first, f.first)));
-        edge.insert(std::make_pair(std::min(e.first, f.second), std::max(e.first, f.second)));
+        edge.insert(make_pair(min(e.first, f.first), max(e.first, f.first)));
+        edge.insert(make_pair(min(e.first, f.second), max(e.first, f.second)));
       }
     }
     face[e.second].clear();
@@ -278,7 +285,7 @@ public:
   void simplify(int target, double threshold) {
     while (faceN > target) {
       printf("%c%d\t", 13, faceN);
-      std::pair<int, int> e = selectEdge(threshold);
+      auto e = selectEdge(threshold);
       removeEdge(e);
       selfCheck();
       fflush(stdout);
